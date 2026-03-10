8d4922c0af7713c23a96f3f5d49adcf6e68d90f1
Mihai <MihaiVasilescu03@gmail.com>
2026-03-11 00:44:33 +0200

    Added data to about page

--- app/about.jsx
+++ app/about.jsx
@@ -1,18 +1,42 @@
-import { getDB } from '../utils/db'
-import { View, Text, StyleSheet, Image, Pressable } from 'react-native';
+import { useEffect, useState } from 'react';
+import { getDB } from '../utils/db';
+import { View, Text, StyleSheet, Image, Pressable, Linking } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function About() {
-    return (
-        <SafeAreaProvider>
-            <View style={aboutStyles.aboutView}>
-                <Image style={aboutStyles.aboutImage} source={require("../assets/aboutgraphic.jpg")} resizeMode="contain"/>
-                <View style={aboutStyles.aboutTextView}>
-                    <Text></Text>
-                </View>
-            </View>
-        </SafeAreaProvider>
-    )
+  const [db, setDb] = useState(null);
+  const [agency, setAgency] = useState(null);
+
+  useEffect(() => {
+    getDB().then(setDb);
+  }, []);
+
+  useEffect(() => {
+    if (!db) return;
+    db.getFirstAsync('SELECT * FROM agency')
+      .then(row => setAgency(row));
+  }, [db]);
+
+  return (
+    <SafeAreaProvider>
+      <View style={aboutStyles.aboutView}>
+        <Image style={aboutStyles.aboutImage} source={require("../assets/aboutgraphic.jpg")} resizeMode="contain"/>
+        <View style={aboutStyles.aboutTextView}>
+          <Text style={aboutStyles.aboutText}>{agency?.agency_name}</Text>
+          <Pressable onPress={() => Linking.openURL(agency?.agency_url)}>
+            <Text style={[aboutStyles.aboutLink, aboutStyles.aboutText]}>{agency?.agency_url}</Text>
+          </Pressable>
+          <Pressable onPress={() => Linking.openURL(`tel:${agency?.agency_phone.replace(/\s/g, '')}`)}>
+            <Text style={[aboutStyles.aboutLink, aboutStyles.aboutText]}>Phone: {agency?.agency_phone}</Text>
+          </Pressable>
+          <Pressable onPress={() => Linking.openURL(`mailto:${agency?.agency_email}`)}>
+            <Text style={[aboutStyles.aboutLink, aboutStyles.aboutText]}>E-mail: {agency?.agency_email}</Text>
+          </Pressable>
+          <Text style={aboutStyles.aboutText}>România, Brașov</Text>
+        </View>
+      </View>
+    </SafeAreaProvider>
+  );
 }
 const aboutStyles = StyleSheet.create({
 
@@ -34,5 +58,15 @@ const aboutStyles = StyleSheet.create({
             flex: 1,
             alignItems: 'center',
             backgroundColor: 'green'
+        },
+        
+        aboutText: {
+            fontSize: 20,
+            fontWeight: 'bold',
+        },
+
+        aboutLink: {
+            color: 'blue',
+            textDecorationLine: 'underline',
         }
     });
